8ba05b3e0e122c0caef5fce58d6a983332963c0a
Jessiiiiiiie <siranzhang1202@gmail.com>
2020-05-03 16:54:53 +0800

    first commit

---
+++ js/login.js
@@ -0,0 +1,12 @@
+function check_login()
+{
+	
+	var name=$("#user_name").val();
+	var pass=$("#password").val();
+	if(name=='2016307070306' && pass=='000000')
+	{
+		//alert('success');
+		//window.location.href = "./1.html"./Highcharts-6.1.0/index.htm;
+		window.open("./Highcharts-6.1.0/index.htm");
+	}
+}
